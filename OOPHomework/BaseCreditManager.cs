57f329766309b54f62400675e0f5acb24d784c78
Graig <Graig@DESKTOP-M2SO4IV>
2024-03-15 20:53:02 +0300

    Add project files.

---
+++ OOPHomework/BaseCreditManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPHomework
+{
+        public abstract class BaseCreditManager : ICreditManager
+    {
+        public abstract void Calculate(); // Calculate operasyonu değişken olduğu için abstract yazıyoruz ve
+                                          // sadece imzasını alıyoruz.
+        
+        public virtual void Save()
+        {
+            Console.WriteLine("Kaydedildi");
+        }
+    }
+}
---
+++ OOPHomework/MilitaryCreditManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPHomework
+{
+    public class MilitaryCreditManager : BaseCreditManager, ICreditManager
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Asker kredisi hesaplandı.");
+        }
+
+        //public void Save()  Save operasyonunu BaseCreditManager'dan çekeceği için gerek kalmayacak 
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
